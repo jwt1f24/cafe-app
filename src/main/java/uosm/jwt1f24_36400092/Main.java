400ea08b59d488d1da25daee13095c19e121b178
Jun Wei <jwpro637@gmail.com>
2026-03-22 21:02:16 +0800

    Added database setup when initializing

--- src/main/java/uosm/jwt1f24_36400092/Main.java
+++ src/main/java/uosm/jwt1f24_36400092/Main.java
@@ -1,25 +1,22 @@
 package uosm.jwt1f24_36400092;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.layout.*;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    // setup gui
     public void start(Stage primaryStage) throws Exception {
+        // create database if it doesn't exist, otherwise ignore
+        SetupDB.setup();
         // get GUI elements from fxml file
-        FXMLLoader source = new FXMLLoader(getClass().getResource("/main-view.fxml"));
-        // setup scene + stage
-        Scene scene = new Scene(source.load(), 800, 600);
-        primaryStage.setTitle("Cafe App");
-        primaryStage.setScene(scene);
+        FXMLLoader loadWindow = new FXMLLoader(getClass().getResource("/login.fxml"));
+        // setup scene + stage + resizing limits
+        Scene loginScene = new Scene(loadWindow.load(), 800, 600);
+        primaryStage.setTitle("Login");
+        primaryStage.setMinHeight(400);
+        primaryStage.setMinWidth(500);
+        primaryStage.setScene(loginScene);
         primaryStage.show();
     }
     // instantiate the program
