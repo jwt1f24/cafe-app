ce2e61d3c529d218a3378d5a2c3c655e2a7f4e84
Jun Wei <jwpro637@gmail.com>
2026-03-22 21:04:27 +0800

    Added database access for account matching

--- src/main/java/uosm/jwt1f24_36400092/LoginControls.java
+++ src/main/java/uosm/jwt1f24_36400092/LoginControls.java
@@ -1,16 +1,11 @@
 package uosm.jwt1f24_36400092;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
-import javafx.scene.Parent;
-import javafx.scene.Node;
-import javafx.scene.Scene;
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import javafx.scene.*;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import java.io.IOException;
+import java.sql.*;
 
 public class LoginControls {
     @FXML
@@ -19,17 +14,13 @@ public class LoginControls {
     private PasswordField passField;
     @FXML
     private Label alert;
-    @FXML
-    private Button submitBtn;
-    @FXML
-    private Button toSignUp;
 
     // login event handling
     @FXML
-    private void login() {
+    private void login(ActionEvent event) {
         String username = nameField.getText();
         String password = passField.getText();
-
+        // input validation
         if (username.isEmpty()) {
             alert.setText("Please fill in username!");
             return;
@@ -38,6 +29,47 @@ public class LoginControls {
             return;
         } else {
             alert.setText(" ");
+        }
+        // search if input matches any database entry in users table
+        String sql = "SELECT * FROM users WHERE username = ? AND password = ?;";
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/cafe", "root", "");
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, username);
+            statement.setString(2, password);
+            // if credentials match an entry, login is successful
+            ResultSet result = statement.executeQuery();
+            if (result.next()) {
+                String role = result.getString("role");
+                alert.setText("Login successful!");
+                if (role.equals("admin")) {
+                    try {
+                        // if account is an admin
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/admindashboard.fxml"));
+                        Parent root = loader.load();
+                        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                        stage.setScene(new Scene(root));
+                        stage.setTitle("Admin Dashboard");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    try {
+                        // if account is a user (default)
+                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/userdashboard.fxml"));
+                        Parent root = loader.load();
+                        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                        stage.setScene(new Scene(root));
+                        stage.setTitle("User Dashboard");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } else {
+                alert.setText("Invalid! Either incorrect credentials or account does not exist.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
     // switch to sign up page
