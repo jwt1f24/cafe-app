b7e9b2e18be1c1aed435fdf3f0f8c86f3406e743
Jun Wei <jwpro637@gmail.com>
2026-03-22 21:01:11 +0800

    Added syntax for database & table setup

--- src/main/java/uosm/jwt1f24_36400092/SetupDB.java
+++ src/main/java/uosm/jwt1f24_36400092/SetupDB.java
@@ -1,4 +1,29 @@
 package uosm.jwt1f24_36400092;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.SQLException;
 
 public class SetupDB {
+    public static void setup() throws SQLException {
+        // create database if it doesn't exist, otherwise ignore
+        try (Connection connection = DriverManager.getConnection(
+                "jdbc:mysql://127.0.0.1:3306/", "root", "");
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("CREATE DATABASE IF NOT EXISTS cafe;");
+        }
+        // create tables if it doesn't exist, otherwise ignore
+        try (Connection connection = DriverManager.getConnection(
+                    "jdbc:mysql://127.0.0.1:3306/cafe", "root", "");
+             Statement statement = connection.createStatement()) {
+            statement.executeUpdate("""
+                    CREATE TABLE IF NOT EXISTS users (
+                        userid INT PRIMARY KEY AUTO_INCREMENT,
+                        username VARCHAR(50) UNIQUE NOT NULL,
+                        password VARCHAR(255) NOT NULL,
+                        role VARCHAR(25) NOT NULL DEFAULT 'user',
+                        CHECK(role IN ('user','admin')));
+                    """);
+        }
+    }
 }
