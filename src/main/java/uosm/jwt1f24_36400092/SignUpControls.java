de9ff427141959914a9219725753bbc042474ed0
Jun Wei <jwpro637@gmail.com>
2026-03-22 15:03:42 +0800

    Fixes some bugs

--- src/main/java/uosm/jwt1f24_36400092/SignUpControls.java
+++ src/main/java/uosm/jwt1f24_36400092/SignUpControls.java
@@ -1,5 +1,55 @@
 package uosm.jwt1f24_36400092;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import java.io.IOException;
 
 public class SignUpControls {
+    @FXML
+    private TextField nameField;
+    @FXML
+    private PasswordField passField;
+    @FXML
+    private Label alert;
+    @FXML
+    private Button submitBtn;
+    @FXML
+    private Button toLogin;
 
+    // sign up event handling
+    @FXML
+    private void signUp() {
+        String username = nameField.getText();
+        String password = passField.getText();
+
+        if (username.isEmpty()) {
+            alert.setText("Please fill in username!");
+            return;
+        } else if (password.isEmpty()) {
+            alert.setText("Please fill in password!");
+            return;
+        } else {
+            alert.setText(" ");
+        }
+    }
+    // switch to login page
+    @FXML
+    private void loginPage(ActionEvent event) throws IOException {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.setTitle("Login");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
